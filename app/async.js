e6caaddb6ebeae0e8569f350e3a9152c28466e3a
Cesar Mandamiento <cesar.mandamiento@beauthentic.digital>
2021-01-19 17:38:04 -0500

    fix(async): fix assertions issues

--- app/async.js
+++ app/async.js
@@ -1,11 +1,15 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(value), 10);
+    });
   },
 
-  manipulateRemoteData: function(url) {
-
-  }
+  manipulateRemoteData: function (url) {
+    return fetch(url)
+      .then((response) => response.json())
+      .then(({ people }) => people.map((person) => person.name).sort());
+  },
 };
