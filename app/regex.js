2adf2575e5dd707a5e8ad740a1532ca0ebbcbd2b
Cesar Mandamiento <cesar.mandamiento@beauthentic.digital>
2021-01-19 17:51:51 -0500

    fix(regex): fix assertion issues

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,26 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
-
+  containsNumber: function (str) {
+    return /\d/.test(str);
   },
 
-  containsRepeatingLetter: function(str) {
-
+  containsRepeatingLetter: function (str) {
+    return /([A-Za-z])\1/.test(str);
   },
 
-  endsWithVowel: function(str) {
-
+  endsWithVowel: function (str) {
+    return /[aeiou]$/i.test(str);
   },
 
-  captureThreeNumbers: function(str) {
-
+  captureThreeNumbers: function (str) {
+    const match = /\d{3}/.exec(str);
+    return match ? match[0] : false;
   },
 
-  matchesPattern: function(str) {
-
+  matchesPattern: function (str) {
+    return /^\d{3}-\d{3}-\d{4}$/.test(str);
   },
 
-  isUSD: function(str) {
-
-  }
+  isUSD: function (str) {},
 };
