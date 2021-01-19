31c3ed4dba1cf9e5a911651f51cf653187961306
Cesar Mandamiento <cesar.mandamiento@beauthentic.digital>
2021-01-19 18:23:24 -0500

    wip(numbers): wip on multiply tests

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
-
+  valueAtBit: function (num, bit) {
+    return 1 & (num >> (bit - 1));
   },
 
-  base10: function(str) {
-
+  base10: function (str) {
+    return parseInt(str, 2);
   },
 
-  convertToBinary: function(num) {
-
+  convertToBinary: function (num) {
+    const VALUES = [];
+    for (let i = 7; i > -1; i--) {
+      VALUES.push(num & (1 << i) ? 1 : 0);
+    }
+    return VALUES.join('');
   },
 
-  multiply: function(a, b) {
-
-  }
+  multiply: function (a, b) {},
 };
